6e839430f47f537b0a82bb41f25554bb4d07ffbd
RavishaJain <40623176+RavishaJain@users.noreply.github.com>
2022-08-15 18:25:09 +0530

    Add files via upload

---
+++ codes/exrand.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "coeffs.h"
+
+int  main(void) //main function begins
+{
+ 
+//Uniform random numbers
+triangular("tri.dat", 1000000);
+
+
+return 0;
+}
